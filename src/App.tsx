5ff8e05c40ee3b24412c0726e3b65a3f87903e07
Saga-sanga <vulcan248@gmail.com>
2022-12-06 13:35:33 +0530

    feat: Add style to first half of page

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,8 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="">
-      <div className=''>
+    <div className="text-dark-blue">
+      <div className='bg-[url(./assets/bg-tablet-pattern.svg)] bg-no-repeat bg-cover'>
         <nav className='flex justify-between'>
           <div>
             <img src={logo} alt="Manage Logo" />
@@ -34,54 +34,60 @@ function App() {
           <a className='hidden md:block' href="">Get Started</a>
           <div className='md:hidden'><a href=""><img src={hamburger} alt="menu icon" /></a></div>
         </nav>
+        <img className='w-full' src={illustrationIntro} alt="charts and percentages image" />
       </div>
 
-      <div >
-        <div className='bg-[url(./assets/bg-tablet-pattern.svg)] bg-no-repeat bg-cover'>
-          <img className='w-full' src={illustrationIntro} alt="charts and percentages image" />
-          <h1 className='text-4xl font-bold leading-tight text-center'>Bring everyone together to build better products.</h1>
-          <p>
-            Manage makes it simple for software teams to plan day-to-day
-            tasks while keeping the larger team goals in view.
-          </p>
-          <a href="">Get Started</a>
-        </div>
+      <section >
+        <article className='flex flex-col items-center text-center bg-[url(./assets/bg-tablet-pattern.svg)] bg-no-repeat bg-cover'>
+          <div>
+            <h1 className='text-4xl font-bold leading-tight'>Bring everyone together to build better products.</h1>
+            <p className='text-dark-grayish-blue font-normal'>
+              Manage makes it simple for software teams to plan day-to-day
+              tasks while keeping the larger team goals in view.
+            </p>
+            <a className='text-very-light-gray font-medium text-xs bg-bright-red px-8 py-3 rounded-full cursor-pointer' href="">Get Started</a>
+          </div>
+
+          <div>
+            <h2 className='text-3xl font-bold leading-tight'>What’s different about Manage?</h2>
+            <p className='text-dark-grayish-blue text-sm'>
+              Manage provides all the functionality your team needs, without
+              the complexity. Our software is tailor-made for modern digital
+              product teams.
+            </p>
+          </div>
+        </article>
+
         <div>
-          <h2>What’s different about Manage?</h2>
-          <p>
-            Manage provides all the functionality your team needs, without
-            the complexity. Our software is tailor-made for modern digital
-            product teams.
-          </p>
-          <h3>
+          <h3 className='font-bold'>
             <span>01</span>
             Track company-wide progress
           </h3>
-          <p>
+          <p className='text-dark-grayish-blue text-sm'>
             See how your day-to-day tasks fit into the wider vision. Go from
             tracking progress at the milestone level all the way done to the
             smallest of details. Never lose sight of the bigger picture again.
           </p>
-          <h3>
+          <h3 className='font-bold'>
             <span>02</span>
             Advanced built-in reports
           </h3>
-          <p>
+          <p className='text-dark-grayish-blue text-sm'>
             Set internal delivery estimates and track progress toward company
             goals. Our customisable dashboard helps you build out the reports
             you need to keep key stakeholders informed.
           </p>
-          <h3>
+          <h3 className='font-bold'>
             <span>03</span>
             Everything you need in one place
           </h3>
-          <p>
+          <p className='text-dark-grayish-blue text-sm'>
             Stop jumping from one service to another to communicate, store files,
             track tasks and share documents. Manage offers an all-in-one team
             productivity solution.
           </p>
         </div>
-      </div>
+      </section>
 
       <div>
         <h3>What they’ve said</h3>
