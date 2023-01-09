96327efe6798e0670e504e784cf19fde1402e6dc
Saga-sanga <vulcan248@gmail.com>
2023-01-09 20:24:26 +0530

    feat: Add open close function to navigation

--- src/components/HeaderNav.tsx
+++ src/components/HeaderNav.tsx
@@ -1,13 +1,23 @@
 import logo from '../assets/logo.svg';
 import hamburger from '../assets/icon-hamburger.svg';
+import close from '../assets/icon-close.svg';
 import illustrationIntro from '../assets/illustration-intro.svg';
+import { useState } from 'react';
 
 export default function HeaderNav() {
+  const [imgSrc, setImgSrc] = useState(hamburger);
+  const [divPos, setDivPos] = useState("translate-y-[-50%]");
+
+  function handleClick() {
+    imgSrc === hamburger ? setImgSrc(close) : setImgSrc(hamburger);
+    divPos === "translate-y-[-50%]" ? setDivPos("translate-y-[110%]") : setDivPos("translate-y-[-50%]");
+  }
+
   return (
     <div className='bg-[url(./assets/bg-tablet-pattern.svg)] bg-no-repeat bg-cover bg-[3rem_-5rem] px-4 pb-4'>
-        <nav className='flex relative justify-between items-center px-2 py-10'>
+        <nav className='flex relative justify-between items-end px-2 py-10'>
           <img className='w-28' src={logo} alt="Manage Logo" />
-          <div className='absolute left-2/4 top-24 ml-[-45vw] md:flex'>
+          <div id='mobile-nav' className={`absolute left-2/4 ${divPos} ml-[-45vw] md:flex transition-transform duration-300`}>
             <ul className='flex flex-col items-center gap-6 w-[90vw] bg-white rounded p-12 font-bold'>
               <li>Pricing</li>
               <li>Product</li>
@@ -18,7 +28,7 @@ export default function HeaderNav() {
           </div>
 
           <a className='hidden md:block' href="">Get Started</a>
-          <div className='md:hidden'><a href=""><img src={hamburger} alt="menu icon" /></a></div>
+          <div className='md:hidden'><img src={imgSrc} alt="menu icon" height={21} onClick={handleClick} /></div>
         </nav>
         <img className='w-full' src={illustrationIntro} alt="charts and percentages image" />
       </div>
