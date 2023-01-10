3def745993bcffb061dc19a815c03d1044f1f599
Saga-sanga <vulcan248@gmail.com>
2023-01-10 14:26:20 +0530

    feat: Add Desktop Nav bar Items and style

--- src/components/HeaderNav.tsx
+++ src/components/HeaderNav.tsx
@@ -5,31 +5,32 @@ import { useState } from 'react';
 
 export default function HeaderNav() {
   const [imgSrc, setImgSrc] = useState(hamburger);
-  const [divPos, setDivPos] = useState("translate-y-[-50%]");
+  const [divPos, setDivPos] = useState("translate-y-[-65%]");
   const [visibility, setVisibility] = useState("hidden");
 
   function handleClick() {
     if (imgSrc === hamburger) {
       setImgSrc(close);
-      setDivPos("translate-y-[110%]");
+      setDivPos("translate-y-[65%]");
       setVisibility('');
       document.body.classList.add('overflow-hidden');
     } 
     if (imgSrc === close) {
       setImgSrc(hamburger);
-      setDivPos("translate-y-[-50%]");
+      setDivPos("translate-y-[-65%]");
       setVisibility('hidden');
       document.body.classList.remove('overflow-hidden');
     }
   }
+  // className='bg-[url(./assets/bg-tablet-pattern.svg)] bg-no-repeat bg-cover bg-[3rem_-5rem]'
 
   return (
-    <div className='bg-[url(./assets/bg-tablet-pattern.svg)] bg-no-repeat bg-cover bg-[3rem_-5rem] px-4'>
+    <div className='px-4 absolute w-full'>
       <div onClick={handleClick} className={`${visibility} fixed w-screen h-screen modal-overlay scale-110 z-20`}></div>
-      <nav className='flex relative justify-between items-end px-2 py-10 z-30'>
+      <nav className='flex relative justify-between items-center px-2 py-10 z-30'>
         <img className='w-28' src={logo} alt="Manage Logo" />
-        <div id='mobile-nav' className={`absolute left-2/4 ${divPos} ml-[-45vw] md:flex transition-transform duration-300`}>
-          <ul className='flex flex-col items-center gap-6 w-[90vw] bg-white rounded p-12 font-bold'>
+        <div id='mobile-nav' className={`absolute md:static md:ml-0 md:translate-y-0 left-2/4 ${divPos} ml-[-45vw] transition-transform duration-300`}>
+          <ul className='flex flex-col items-center gap-6 md:gap-8 w-[90vw] md:flex-row md:p-0 md:w-auto md:bg-transparent md:text-xs bg-white rounded p-12 font-bold'>
             <li>Pricing</li>
             <li>Product</li>
             <li>About Us</li>
@@ -38,7 +39,7 @@ export default function HeaderNav() {
           </ul>
         </div>
 
-        <a className='hidden md:block text-very-light-gray font-medium text-xs mt-6 bg-bright-red px-8 py-3 rounded-full cursor-pointer' href="">Get Started</a>
+        <a className='hidden md:block text-very-light-gray font-medium text-xs bg-bright-red px-8 py-3 rounded-full cursor-pointer' href="">Get Started</a>
         <div className='md:hidden '><img src={imgSrc} alt="menu/close icon" style={{height: '1.25rem'}} onClick={handleClick} /></div>
       </nav>
     </div>
